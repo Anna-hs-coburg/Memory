4a7ee905f9e741cccb117ac2f053cbea097ff0d8
Anna-hs-coburg <123657225+Anna-hs-coburg@users.noreply.github.com>
2023-02-07 11:11:06 +0100

    first dynamic Memory
    der erste Entwurf das es dynamisch funktioniert, mit Einfügen und umdrehen von Bildern.

--- Memory/Memory.js
+++ Memory/Memory.js
@@ -1,1 +1,25 @@
-console.log("foo")
+console.log("foo");
+
+var cards = ["bunny.jpg", "cow.jpg", "hedgehog.jpg", "lion.jpg", "monkey.jpg", "rhino.jpg", "sheep.jpg", "zebra.jpg", "bunny.jpg", "cow.jpg", "hedgehog.jpg", "lion.jpg", "monkey.jpg", "rhino.jpg", "sheep.jpg", "zebra.jpg"];
+// cards.random()
+let container = document.getElementById("Memory");
+for (let index = 0; index < cards.length; index++) {
+    const element = cards[index];
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'card');
+    var newDiv2 = document.createElement('div');
+    newDiv2.setAttribute('class', 'newDiv2');
+    var newImg = document.createElement('img');
+    newImg.setAttribute('src', './MemoryBilder /' + element);
+    newImg.setAttribute('class', 'picture');
+    newDiv.appendChild(newImg);
+    // newDiv.appendChild(newDiv2);
+    container.appendChild(newDiv);
+    console.log(newDiv);
+    newDiv.addEventListener("click", function (event) {
+        console.log(event);
+        event.target.getElementsByTagName('img')[0].style.visibility = 'visible';
+    })
+}
+
+
