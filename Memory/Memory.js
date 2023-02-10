f2565649503fc293bada297bf651fb5ddedaf493
Anna-hs-coburg <123657225+Anna-hs-coburg@users.noreply.github.com>
2023-02-10 14:01:31 +0100

    Shuffle
    das durchmischen der karten funktioniert

--- Memory/Memory.js
+++ Memory/Memory.js
@@ -1,109 +1,64 @@
 console.log("foo");
 
+var cards = ["bunny.jpg", "cow.jpg", "hedgehog.jpg", "lion.jpg", "monkey.jpg", "rhino.jpg", "sheep.jpg", "zebra.jpg", "bunny.jpg", "cow.jpg", "hedgehog.jpg", "lion.jpg", "monkey.jpg", "rhino.jpg", "sheep.jpg", "zebra.jpg"];
+
 let match = "";
-let element1 = null;
-let element2 = null;
-let highscore = document.getElementById("score1");
+let move1 = null;
+let move2 = null;
+let highscore = 0;
 
-const cards = ["bunny.jpg", "cow.jpg", "hedgehog.jpg", "lion.jpg", "monkey.jpg", "rhino.jpg", "sheep.jpg", "zebra.jpg", "bunny.jpg", "cow.jpg", "hedgehog.jpg", "lion.jpg", "monkey.jpg", "rhino.jpg", "sheep.jpg", "zebra.jpg"];
+function initalize() {
+    let container = document.getElementById("Memory");
 
+    cards = cards.sort((a, b) => 0.5 - Math.random());
 
-//  const cards = [];
+    for (let index = 0; index < cards.length; index++) {
+        const element = cards[index];
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'card');
 
-// // let bild = Math.ceil(Math.random() * 6);
-// let memory = document.getElementById("Memory");
-// // memory.innerText = InitCards;
-// // let counter = 0;
-// // let IntervallID;
+        const newImg = document.createElement('img');
+        newImg.setAttribute('src', './MemoryBilder /' + element);
+        newImg.setAttribute('class', 'picture');
 
-// function shuffle() {
-//     generatRandomPicture();
-//     // setTimeout(compare, 3000);
-// }
+        newDiv.appendChild(newImg);
+        container.appendChild(newDiv);
 
-// function generatRandomPicture() {
-//     let zufall = Math.ceil(Math.random() * 16);
-//     console.log(InitCards);
-//     document.getElementById("Memory");
-//     memory.innerText = zufall;
-//     // counter++;
-//     // if (counter > 7) {
-//     //     // clearInterval(IntervallID);
-//     //     counter = 0;
-//     // }
-// }
+        newDiv.addEventListener("click", function (event) {
+            // console.log(event);
+            var Img = event.target.getElementsByTagName('img')[0];
+            Img.style.visibility = 'visible';
 
-
-
-let container = document.getElementById("Memory");
-
-for (let index = 0; index < cards.length; index++) {
-    const element = cards[index];
-    const newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'card');
-
-    const newDiv2 = document.createElement('div');
-    newDiv2.setAttribute('class', 'newDiv2');
-
-    const newImg = document.createElement('img');
-    newImg.setAttribute('src', './MemoryBilder /' + element);
-    newImg.setAttribute('class', 'picture');
-
-    newDiv.appendChild(newImg);
-    // newDiv.appendChild(newDiv2);
-    container.appendChild(newDiv);
-    // console.log(newDiv);
-
-    newDiv.addEventListener("click", function (event) {
-        // console.log(event);
-        var Img = event.target.getElementsByTagName('img')[0];
-        Img.style.visibility = 'visible';
-
-        if (element1 != null && element2 != null) {
-            element1.style.visibility = 'hidden';
-            element2.style.visibility = 'hidden';
-            element1 = null;
-            element2 = null;
-        }
-
-        if (match == "") {
-            match = Img.getAttribute('src');
-            element1 = Img;
-
-        } else if (match == Img.getAttribute('src')) {
-            console.log("gleich");
-            match = "";
-            highscore.innerText = parseInt(highscore.innerText) + 200;
-
-        } else {
-            console.log("ungleich");
-            match = "";
-            element2 = Img;
-            highscore.innerText = parseInt(highscore.innerText) - 100;
-
-            let pastTime = 0;
-            let timer = setInterval(sec, 3000);
-
-            function sec() {
-                console.log(timer);
-                pastTime++;
-                if (pastTime > 3) {
-                    clearInterval(timer);
-                }
+            if (move1 != null && element2 != null) {
+                move1.style.visibility = 'hidden';
+                move2.style.visibility = 'hidden';
+                move1 = null;
+                move2 = null;
             }
-        }
-    })
+
+            if (match == "") {
+                match = Img.getAttribute('src');
+                move1 = Img;
+
+            } else if (match == Img.getAttribute('src')) {
+                console.log("gleich");
+                match = "";
+                highscore += 200;
+                document.getElementById("score1").innerText = highscore;
+
+            } else {
+                console.log("ungleich");
+                match = "";
+                move2 = Img;
+                highscore -= 100;
+                document.getElementById("score1").innerText = highscore;
+
+            }
+        })
+    }
+
 }
 
-
-
-
-
-// function shuffle() {
-//     cards.forEach(card => {
-//       let ramdomPos = Math.floor(Math.random() * 16);
-//       cards.setAttribute('src', './MemoryBilder /' + element);
-//     })};
 
 
 
